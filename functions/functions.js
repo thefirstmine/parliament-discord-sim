76368a018a7f94719ed57964166bdc3d63a52d36
shannaurelle <shannaurelleg@gmail.com>
2021-08-22 12:33:25 +0800

    cleaning code part 2

--- functions/functions.js
+++ functions/functions.js
@@ -1,58 +1,100 @@
 const PARLIAMENT_SEATS = 101;
 
 function weightedRandom(prob) {
+
   let i, sum=0, r=Math.random();
+
   for (i in prob) {
+
     sum += prob[i];
+
     if (r <= sum) return i;
+
   }
+
 }
 
 function pick_one(list){
   // wrapper function for picking one item in a uniform distribution 
   let weight = 1/list.length, dict = {};
+
   for(item of list){
+
     dict[item] = weight;
+
   }
+
   return weightedRandom(dict);
+
 }
 
 function weightedRandomDistrib(min,max,mean,varianceFactor) {
+
   let prob = [], seq = [];
+
   for(let i=min;i<max;i++) {
+
     prob.push(Math.pow(max-Math.abs(mean-i),varianceFactor));
+
     seq.push(i);
+
   }
+
   return chance.weighted(seq, prob);
+  
 }
 
 function createMinisters(list,issues){
+
   let minister, issue, stance=0, stances={};
+
   for (minister in list){
+
     for (issue in issues) {
+
       stances[issue] = Math.random();
+
     }
+
     let loyalty = Math.random(), disloyalty = 1 - loyalty;
+
     stances["loyalty"] = loyalty;
+
     stances["disloyalty"] = disloyalty;
+
     stances["party"] = "";
-    list[minister] = stances
+
+    list[minister] = stances;
+
     stances = {}
+
   }
-  return list
+
+  return list;
 }
 
 function createParties(parties, issues){
+
   let partyName, issue, stances={};
+
   for (partyName in parties){
+
     let representation = parties[partyName];
+
     for (issue in issues) {
+
       stances[issue] = Math.random();
+
     }
+
     stances["ministers"] = PARLIAMENT_SEATS * representation;
-    parties[partyName] = stances
+
+    parties[partyName] = stances;
+
     stances = {}
+
   }
+
   return parties;
 }
 
@@ -82,7 +124,7 @@ function assignToParties(ministers, parties){
         ministers["minister"]["party"] = partyName;
 
         parties[partyName]["ministers"] -= 1;
-        
+
       }   
   }
 }
